4bef96d5251074dbdbc3693bc36b497d48323f2a
Colm Woodlock <Colm Woodlock>
2018-11-14 19:05:26 +0000

    Started game grid

---
+++ Game/Assets/__Scripts/Board.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour {
+
+    //variables
+    public int width; //width of board
+    public int height; //height of board
+    public GameObject tilePrefab;
+    private BackgroundTile[,] allTiles;
+
+	// Use this for initialization
+	void Start () {
+        allTiles = new BackgroundTile[width, height]; //size of the grid
+        SetUp();
+    }
+	
+	private void SetUp()
+    {
+        for(int i = 0; i < width; i++)
+        {
+            for(int j = 0; j < height; j++)
+            {
+                //Adapted from https://gamedev.stackexchange.com/questions/128398/why-does-the-unity-manual-add-quaternion-identity-to-an-instantiated-object
+                Vector2 tempPosition = new Vector2(i, j);
+                Instantiate(tilePrefab, tempPosition, Quaternion.identity);
+            }
+        }
+    }
+}
