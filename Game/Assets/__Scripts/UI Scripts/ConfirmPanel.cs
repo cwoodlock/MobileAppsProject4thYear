d0da3c22e0bf96dbd091e2eaf3e0a7a53e2aa9ad
Colm Woodlock <Colm Woodlock>
2018-12-11 20:20:26 +0000

    can now click on level to bring up confirm panel

---
+++ Game/Assets/__Scripts/UI Scripts/ConfirmPanel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ConfirmPanel : MonoBehaviour {
+
+    public string levelToLoad;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Cancel()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
---
+++ Game/Assets/__Scripts/UI Scripts/LevelButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelButton : MonoBehaviour {
+
+    public Text levelText;
+    public int level;
+    public GameObject confirmPanel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ConfirmPanel()
+    {
+        confirmPanel.SetActive(true);
+    }
+}
